8029f4bd16a2187c834e4c5958d0ad9819663963
ABHISHEK SAREWAR <77223307+AbhishekSarewar1911@users.noreply.github.com>
2021-08-29 12:49:07 +0530

    VacantBerth UI

---
+++ VacantBerth/www.vacantberth.com/Content/js/site.js
@@ -0,0 +1,17 @@
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+function RegisterUser() {
+    document.getElementById('userRegister').setAttribute('style', 'display:block');
+}
+
+function Login() {
+    document.getElementById('userRegister').setAttribute('style', 'display:none');
+}
+
+//document.getElementById("typeSubmit").onclick = function () {
+//    document.getElementById("submitByLogout").submit();
+//}
